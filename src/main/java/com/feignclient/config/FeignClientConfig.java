c900083fc8d1219a8fa7876faca706040eb10813
Gurnatha Reddy <gurunatha.y@scriptbees.com>
2022-11-30 15:43:11 +0530

    revert test

--- src/main/java/com/feignclient/config/FeignClientConfig.java
+++ src/main/java/com/feignclient/config/FeignClientConfig.java
@@ -5,7 +5,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class FeignClientConfig {
 
 	@Bean
